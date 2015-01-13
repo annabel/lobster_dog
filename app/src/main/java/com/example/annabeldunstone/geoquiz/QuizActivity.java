cb994cae515fe78496fd4359edffd215b4e66e72
Annabel Dunstone <a.dunstone89@gmail.com>
2015-01-12 19:22:04 -0600

    Created TrueFalse class and updated controller

--- app/src/main/java/com/example/annabeldunstone/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/annabeldunstone/geoquiz/QuizActivity.java
@@ -6,6 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -13,11 +14,28 @@ public class QuizActivity extends ActionBarActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+    private Button mPreviousButton;
+    private TextView mQuestionTextView;
+
+    private TrueFalse[] mQuestionBank = new TrueFalse[] {
+        new TrueFalse(R.string.question_lithuania, false),
+        new TrueFalse(R.string.question_czech, true),
+        new TrueFalse(R.string.question_russia, false),
+        new TrueFalse(R.string.question_wales, false),
+        new TrueFalse(R.string.question_ocean, true)
+    };
+
+    private int mCurrentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
+        int question = mQuestionBank[mCurrentIndex].getQuestion();
+        mQuestionTextView.setText(question);
 
         mTrueButton = (Button)findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
@@ -35,6 +53,15 @@ public class QuizActivity extends ActionBarActivity {
                 Toast.makeText(QuizActivity.this,
                         R.string.correct_toast,
                         Toast.LENGTH_SHORT).show();
+            }
+        });
+        mNextButton = (Button)findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.onClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                int question = mQuestionBank[mCurrentIndex].getQuestion();
+                mQuestionTextView.setText(question);
             }
         });
     }
