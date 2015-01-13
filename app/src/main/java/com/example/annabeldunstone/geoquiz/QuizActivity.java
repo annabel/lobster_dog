b333d546b384fdd6fa150e0d7add4cedd8a11b4d
Annabel Dunstone <a.dunstone89@gmail.com>
2015-01-13 09:10:42 -0600

    Refactored, redesigned, removed action bar

--- app/src/main/java/com/example/annabeldunstone/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/annabeldunstone/geoquiz/QuizActivity.java
@@ -1,16 +1,14 @@
 package com.example.annabeldunstone.geoquiz;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
-public class QuizActivity extends ActionBarActivity {
+public class QuizActivity extends Activity {
 
     private Button mTrueButton;
     private Button mFalseButton;
@@ -18,15 +16,34 @@ public class QuizActivity extends ActionBarActivity {
     private Button mPreviousButton;
     private TextView mQuestionTextView;
 
+    private int mCurrentIndex = 0;
+
+
     private TrueFalse[] mQuestionBank = new TrueFalse[] {
-        new TrueFalse(R.string.question_lithuania, false),
-        new TrueFalse(R.string.question_czech, true),
-        new TrueFalse(R.string.question_russia, false),
-        new TrueFalse(R.string.question_wales, false),
-        new TrueFalse(R.string.question_ocean, true)
+            new TrueFalse(R.string.question_lithuania, false),
+            new TrueFalse(R.string.question_czech, true),
+            new TrueFalse(R.string.question_russia, false),
+            new TrueFalse(R.string.question_wales, false),
+            new TrueFalse(R.string.question_ocean, true)
     };
 
-    private int mCurrentIndex = 0;
+
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getQuestion();
+        mQuestionTextView.setText(question);
+    }
+
+    private void checkAnswer(boolean userPressedTrue) {
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
+
+        int messageResId = 0;
+        if (userPressedTrue == answerIsTrue) {
+            messageResId = R.string.correct_toast;
+        } else {
+            messageResId = R.string.incorrect_toast;
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,58 +51,39 @@ public class QuizActivity extends ActionBarActivity {
         setContentView(R.layout.activity_quiz);
 
         mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
-        int question = mQuestionBank[mCurrentIndex].getQuestion();
-        mQuestionTextView.setText(question);
+
 
         mTrueButton = (Button)findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this,
-                        R.string.incorrect_toast,
-                        Toast.LENGTH_SHORT).show();
+                checkAnswer(true);
             }
         });
         mFalseButton = (Button)findViewById(R.id.false_button);
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this,
-                        R.string.correct_toast,
-                        Toast.LENGTH_SHORT).show();
+                checkAnswer(false);
             }
         });
         mNextButton = (Button)findViewById(R.id.next_button);
-        mNextButton.setOnClickListener(new View.onClickListener() {
+        mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
-                int question = mQuestionBank[mCurrentIndex].getQuestion();
-                mQuestionTextView.setText(question);
+                updateQuestion();
             }
         });
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_quiz, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        mPreviousButton = (Button)findViewById(R.id.previous_button);
+        mPreviousButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + (mQuestionBank.length - 1)) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+        updateQuestion();
     }
 }
+
